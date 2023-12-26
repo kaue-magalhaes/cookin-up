20e5f2feef5e4acbd4c94e66a3703288a8c3b2fd
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2023-12-26 19:22:55 -0300

    Personalizando cards com props

---
+++ src/components/CardCategoria.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import type ICategoria from '@/interfaces/ICategoria';
+import type { PropType } from 'vue';
+
+export default {
+    props: {
+        categoria: {
+            type: Object as PropType<ICategoria>,
+            required: true
+        }
+    }
+}
+</script>
+<template>
+    {{ categoria.nome }}
+    {{ categoria.imagem }}
+</template>
--- src/components/SelecionarIngredientes.vue
+++ src/components/SelecionarIngredientes.vue
@@ -1,17 +1,18 @@
 <script lang="ts">
 import { obterCategorias } from '@/http/index';
 import type ICategoria from '@/interfaces/ICategoria';
+import CardCategoria from './CardCategoria.vue';
 
 export default {
   data() {
     return {
       categorias: [] as ICategoria[]
-    }
+    };
   },
-
   async created() {
     this.categorias = await obterCategorias();
-  }
+  },
+  components: { CardCategoria }
 }
 </script>
 
@@ -26,7 +27,7 @@ export default {
 
     <ul class="categorias">
       <li v-for="categoria in categorias" :key="categoria.nome">
-        {{ categoria.nome }}
+        <CardCategoria :categoria="categoria" />
       </li>
     </ul>
 
@@ -71,5 +72,4 @@ export default {
     margin-bottom: 2.5rem;
   }
 }
-
 </style>
