703f924798a55068c2a2c2b15f543ef699f225ec
Kauê de Magalhães Brandão <kauedemagalhaes27@gmail.com>
2023-12-26 07:30:36 -0300

    Repetindo itens com v-for

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script lang="ts">
   import Banner from './components/Banner.vue';
+import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
 
   export default {
     components: {
-      Banner: Banner
-    }
+    Banner: Banner,
+    ConteudoPrincipal
+}
   }
 </script>
 
 <template>
   <Banner />
+  <ConteudoPrincipal />
 </template>
 
 <style scoped>
