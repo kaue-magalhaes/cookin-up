8357a0641583591887de0616afd1d6037a9c2c28
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2023-12-26 19:06:20 -0300

    Re-renderizando a tela com estado

--- src/components/SelecionarIngredientes.vue
+++ src/components/SelecionarIngredientes.vue
@@ -1,11 +1,16 @@
 <script lang="ts">
 import { obterCategorias } from '@/http/index';
+import type ICategoria from '@/interfaces/ICategoria';
 
 export default {
   data() {
     return {
-      categorias: obterCategorias()
+      categorias: [] as ICategoria[]
     }
+  },
+
+  async created() {
+    this.categorias = await obterCategorias();
   }
 }
 </script>
