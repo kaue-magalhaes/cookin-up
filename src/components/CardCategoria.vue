77d1ac2d6a6e814fb94d6b74dd079f975d067fcb
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2023-12-27 04:30:25 -0300

    Controlando visual com estado

--- src/components/CardCategoria.vue
+++ src/components/CardCategoria.vue
@@ -1,7 +1,7 @@
 <script lang="ts">
 import type ICategoria from '@/interfaces/ICategoria';
 import type { PropType } from 'vue';
-import Tag from './Tag.vue';
+import IngredienteSelecionavel from './IngredienteSelecionavel.vue';
 
 export default {
   props: {
@@ -10,7 +10,7 @@ export default {
       required: true
     }
   },
-  components: { Tag }
+  components: { IngredienteSelecionavel }
 }
 </script>
 <template>
@@ -25,7 +25,7 @@ export default {
 
     <ul class="categoria__ingredientes">
       <li v-for="ingrediente in categoria.ingredientes" :key="ingrediente">
-        <Tag :texto="ingrediente" />
+        <IngredienteSelecionavel :ingrediente="ingrediente" />
       </li>
     </ul>
   </article>
---
+++ src/components/IngredienteSelecionavel.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+import Tag from './Tag.vue';
+
+export default {
+    props: {
+        ingrediente: {
+            type: String,
+            required: true
+        }
+    },
+    components: {
+        Tag
+    },
+    data() {
+        return {
+            selecionado: false
+        }
+    },
+}
+</script>
+
+<template>
+    <button class="ingrediente" @click="selecionado = !selecionado" :aria-pressed="selecionado">
+        <Tag :texto="ingrediente" :ativa="selecionado" />
+    </button>
+</template>
+
+<style scoped>
+.ingrediente {
+    cursor: pointer;
+}
+</style>
